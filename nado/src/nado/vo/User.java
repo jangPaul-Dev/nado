8fcf639e2778ce9200969a163797bbad0f3e5e66
jyj <yohojyj@gmail.com>
2021-01-27 14:46:19 +0900

    user uBirth 리턴값 수정

--- nado/src/nado/vo/User.java
+++ nado/src/nado/vo/User.java
@@ -17,86 +17,72 @@ public class User {
 	public int getuNo() {
 		return uNo;
 	}
-
 	public User setuNo(int uNo) {
 		this.uNo = uNo;
 		return this;
 	}
-
 	public String getuId() {
 		return uId;
 	}
-
 	public User setuId(String uId) {
 		this.uId = uId;
 		return this;
 	}
-
 	public String getuPwd() {
 		return uPwd;
 	}
-
 	public User setuPwd(String uPwd) {
 		this.uPwd = uPwd;
 		return this;
 	}
-
 	public String getuName() {
 		return uName;
 	}
-
 	public User setuName(String uName) {
 		this.uName = uName;
-		return this;
+		return this;	
 	}
 	
 	public Date getuBirth() {
 		return uBirth;
 	}
-	public void setuBirth(Date uBirth) {
+	
+	public User setuBirth(Date uBirth) {
 		this.uBirth = uBirth;
+		return this;
 	}
 
 	public String getuSex() {
 		return uSex;
 	}
-
 	public User setuSex(String uSex) {
 		this.uSex = uSex;
 		return this;
 	}
-
 	public int getPhoneNum() {
 		return phoneNum;
 	}
-
 	public User setPhoneNum(int phoneNum) {
 		this.phoneNum = phoneNum;
 		return this;
 	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public User setEmail(String email) {
 		this.email = email;
 		return this;
 	}
-
 	public String getAddress() {
 		return address;
 	}
-
 	public User setAddress(String address) {
 		this.address = address;
 		return this;
 	}
-
 	public String getUseYn() {
 		return useYn;
 	}
-
 	public User setUseYn(String useYn) {
 		this.useYn = useYn;
 		return this;
